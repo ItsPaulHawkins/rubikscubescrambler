8a79b96cc97f28e648579ac7c723e0760f59575e
ItsPaulHawkins <hawkinsanthonypaul@gmail.com>
2017-02-08 22:14:47 -0500

    Finally got everything together. It generates a random scramble based on the length you give it.

--- rubikscubescrambler/rubikscubescrambler/Program.cs
+++ rubikscubescrambler/rubikscubescrambler/Program.cs
@@ -10,77 +10,98 @@ namespace rubikscubescrambler
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("How long do you want your scramble to be?");
-            int length = Int32.Parse(Console.ReadLine());
-            int[] rannumbers; //creates random number string
-            string[] scramble;
-            scramble = new string[length];
-            rannumbers = new int[length];
-            int count = 1; //initialized to count how many generates there have been
-            Random rnd = new Random();
-            while(count != length)
+            bool loop = true;
+            while (loop == true)
             {
-                int randomnumber = rnd.Next(1, 12);
-                rannumbers[count] = randomnumber;
-                if(rannumbers[count] == 1)
+                Console.WriteLine("How long do you want your scramble to be?");
+                int length = Int32.Parse(Console.ReadLine());
+                int[] rannumbers; //creates random number string
+                string[] scramble;
+                scramble = new string[length];
+                rannumbers = new int[length];
+                int count = 0; //initialized to count how many generates there have been
+                Random rnd = new Random();
+                while (count != length)
                 {
-                    scramble[count] = "R";
-                    count++;
-                }else if(rannumbers[count] == 2)
-                {
-                    scramble[count] = "Ri";
-                    count++;
-                }else if(rannumbers[count] == 3){
-                    scramble[count] = "L";
-                    count++;
-                }else if(rannumbers[count] == 4)
-                {
-                    scramble[count] = "Li";
-                    count++;
-                }else if(rannumbers[count] == 5)
-                {
-                    scramble[count] = "B";
-                    count++;
-                }else if(rannumbers[count] == 6)
-                {
-                    scramble[count] = "Bi";
-                    count++;
-                }else if(rannumbers[count] == 7)
-                {
-                    scramble[count] = "D";
-                    count++;
-                }else if(rannumbers[count] == 8)
-                {
-                    scramble[count] = "Di";
-                    count++;
-                }else if(rannumbers[count] == 9)
-                {
-                    scramble[count] = "F";
-                    count++;
-                }else if(rannumbers[count] == 10)
-                {
-                    scramble[count] = "Fi";
-                    count++;
-                }else if(rannumbers[count] == 11)
-                {
-                    scramble[count] = "U";
-                    count++;
-                }else if(rannumbers[count] == 12)
-                {
-                    scramble[count] = "Ui";
-                    count++;
+                    int randomnumber = rnd.Next(1, 12);
+                    rannumbers[count] = randomnumber;
+                    if (rannumbers[count] == 1)
+                    {
+                        scramble[count] = "R";
+                        count++;
+                    }
+                    else if (rannumbers[count] == 2)
+                    {
+                        scramble[count] = "Ri";
+                        count++;
+                    }
+                    else if (rannumbers[count] == 3)
+                    {
+                        scramble[count] = "L";
+                        count++;
+                    }
+                    else if (rannumbers[count] == 4)
+                    {
+                        scramble[count] = "Li";
+                        count++;
+                    }
+                    else if (rannumbers[count] == 5)
+                    {
+                        scramble[count] = "B";
+                        count++;
+                    }
+                    else if (rannumbers[count] == 6)
+                    {
+                        scramble[count] = "Bi";
+                        count++;
+                    }
+                    else if (rannumbers[count] == 7)
+                    {
+                        scramble[count] = "D";
+                        count++;
+                    }
+                    else if (rannumbers[count] == 8)
+                    {
+                        scramble[count] = "Di";
+                        count++;
+                    }
+                    else if (rannumbers[count] == 9)
+                    {
+                        scramble[count] = "F";
+                        count++;
+                    }
+                    else if (rannumbers[count] == 10)
+                    {
+                        scramble[count] = "Fi";
+                        count++;
+                    }
+                    else if (rannumbers[count] == 11)
+                    {
+                        scramble[count] = "U";
+                        count++;
+                    }
+                    else if (rannumbers[count] == 12)
+                    {
+                        scramble[count] = "Ui";
+                        count++;
+                    }
+
+
                 }
-                foreach (var item in scramble)
+                Console.Write(string.Join(" " , scramble));
+                Console.WriteLine(", Would you like to generate a new one?");
+                string question = Console.ReadLine();
+                if (question == "yes")
                 {
-                    Console.Write(item.ToString());
+                    loop = true;
                 }
-                Console.ReadLine();
+                else
+                {
+                    loop = false;
+                }
+
 
             }
-            
-
-            
-
         }
     }
 }
